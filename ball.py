208d334f846d7f00f4289964ac97444a5aa44a4e
tushara19 <83689012+tushara19@users.noreply.github.com>
2021-11-21 16:48:22 +0530

    ping pong ball

---
+++ ball.py
@@ -0,0 +1,27 @@
+import pygame
+from random import randint
+BLACK = (0,0,0)
+ 
+class Ball(pygame.sprite.Sprite):
+    
+    
+    def __init__(self, color, width, height):
+        
+        super().__init__()
+    
+        self.image = pygame.Surface([width, height])
+        #self.image.fill(BLACK)
+        #self.image.set_colorkey(BLACK)
+ 
+        pygame.draw.rect(self.image, color, [0, 0, width, height])
+        
+        self.velocity = [randint(2,4),randint(-1,4)]
+        self.rect = self.image.get_rect()
+        
+    def update(self):
+        self.rect.x += self.velocity[0]
+        self.rect.y += self.velocity[1]
+
+    def bounce(self):
+        self.velocity[0] = -self.velocity[0]
+        self.velocity[1] = randint(-4,4)
